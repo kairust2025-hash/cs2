ca8f9841be469faefe5f378cadcfa562caa43fa8
kaiiiiiiiiiiii777 <kairust2025@gmail.com>
2026-03-27 17:27:19 +0000

    Update users.js

--- users.js
+++ users.js
@@ -1,2 +1,16 @@
-const auth = { username: "myuser", password: "mypassword" };
-const auth = { username: "myuser", 122: "axis" };
+// users.js
+const users = [
+  { uuid: "", username: "axis", password: "axis" },
+  { uuid: "b8e1d7c2-6a3b-4f2d-8e6a-2c3d4e5f6b72", username: "user02", password: "pass02" },
+  { uuid: "",                                      username: "user03", password: "pass03" }, // not linked yet
+  { uuid: "c7d0c6b3-5a4c-4d3e-9d5b-3d4e5f6a7c83", username: "user04", password: "pass04" },
+  { uuid: "",                                      username: "user05", password: "pass05" }, // not linked yet
+  { uuid: "d6c9b5a4-4b5d-4c4f-8c4a-4e5f6a7b8d94", username: "user06", password: "pass06" },
+  { uuid: "e5b8a4d3-3c6e-4b5a-9b3c-5f6a7b8c9ea5", username: "user07", password: "pass07" },
+  { uuid: "",                                      username: "user08", password: "pass08" }, // not linked yet
+  { uuid: "f4a7b3c2-2d7f-4a6b-8a2d-6a7b8c9d0fb6", username: "user09", password: "pass09" },
+  { uuid: "09b6c2d1-1e8a-4f7c-9f1e-7b8c9d0a1gc7", username: "user10", password: "pass10" }
+];
+
+module.exports = { users };
+// or: export default users;
